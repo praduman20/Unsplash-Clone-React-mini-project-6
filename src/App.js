6d526cdb3df386991a7ca532c65048861defa286
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2023-09-21 12:02:26 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState([]);
+
+  const fetchImages = () => {
+    fetch(
+      `https://api.unsplash.com/search/photos/?client_id=xU0_klGSGPimiwAqjlud8TIiX9yDjLfNBWisIeYslA8&query=${value}&orientation=squarish&per_page=9`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.results.length === 0) {
+          alert("The requested resource doesn’t exist");
+        }
+        setResult(data.results);
+      });
+
+    setValue("");
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      fetchImages();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mydiv">
+        <span style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+          UnDab &copy;
+        </span>
+        <input
+          className="pic-value"
+          type="text"
+          style={{ width: "50%", height: "25px" }}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Search high-resolution pics"
+          onKeyDown={handleKeyDown}
+          autoFocus
+        />
+        <button className="search-btn" onClick={() => fetchImages()}>
+          SEARCH
+        </button>
+      </div>
+      <div className="gallery">
+        {result.map((item) => {
+          return (
+            <img
+              className="item"
+              key={item.id}
+              src={item.urls.regular}
+              alt="Fetched pic"
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
